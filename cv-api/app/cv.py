5a5d1789b17959a0349cb6929f6ae04ab10a26cf
thelongrunsmoke <thelongrunsmoke@gmail.com>
2017-12-28 11:04:52 +0500

    New autoexposure algorithm

--- cv-api/app/cv.py
+++ cv-api/app/cv.py
@@ -12,17 +12,14 @@ class CV:
         self.feed = cv2.VideoCapture(0)
         self.feed.set(cv2.CAP_PROP_FRAME_WIDTH,800)
         self.feed.set(cv2.CAP_PROP_FRAME_HEIGHT,600)
-        # self.feed.set(cv2.CAP_PROP_SHARPNESS,1)
         self.feed.set(cv2.CAP_PROP_AUTO_EXPOSURE,0.25) # manual mode
-        self.feed.set(cv2.CAP_PROP_EXPOSURE, 0)
+        self.feed.set(cv2.CAP_PROP_EXPOSURE, 0.5)
         self.isUpdated = False
         self.time = 0
         self.fps = 0
         self.streamBuf = None
-        self.avg = DequeAverager(3)
-        
-        self.bgSubtractor = cv2.createBackgroundSubtractorMOG2()
-        self.history = 100
+
+        self.skipCounter=0
         
         self.loop = asyncio.get_event_loop()
         self.pool = ThreadPoolExecutor(max_workers=multiprocessing.cpu_count())
@@ -39,39 +36,50 @@ class CV:
     def process(self):
         while True:
             if self.feed.grab() == True:
-                
-                if(self.time == int(time.time())):
-                    self.fps += 1
-                else:
-                    print('FPS: %i' % (self.fps))
-                    self.time = int(time.time())
-                    self.fps=0
+
+                self.countFPS()
             
                 rval, frame = self.feed.retrieve()
 
                 self.autoExposure(cv2.cvtColor(frame, cv2.COLOR_BGR2YUV))
 
-                #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-                mask = self.bgSubtractor.apply(frame, learningRate=1.0/self.history)
-                mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
-                
+##                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)                
+##
+##                img_dft = cv2.dft(numpy.float32(gray),flags=cv2.DFT_COMPLEX_OUTPUT)
+##                magnitude, angle = cv2.cartToPolar(img_dft[:, :, 0], img_dft[:, :, 1])
+##                log_ampl = numpy.log10(magnitude.clip(min=1e-9))
+##                log_ampl_blur = cv2.blur(log_ampl, (3, 3))
+##                magn = numpy.exp(log_ampl-log_ampl_blur)
+##                img_dft[:, :, 0], img_dft[:, :, 1] = cv2.polarToCart(magn, angle)
+##                img_combined = cv2.idft(img_dft)
+##                sal, _ = cv2.cartToPolar(img_combined[:, :, 0], img_combined[:, :, 1])
+##
+##                sal = cv2.GaussianBlur(sal,(5,5),sigmaX=8, sigmaY=0)
+##
+##                sal = sal**2
+##                sal = numpy.float32(sal)/numpy.max(sal)
+##                sal = cv2.resize(sal, frame.shape[1::-1])
+            
+                #colorSal = cv2.cvtColor(sal,cv2.COLOR_GRAY2RGB)
                                 
                 if not self.isUpdated:
                     ret, self.imageBuf = cv2.imencode('*.jpg', frame)
-                    ret, self.imageBuf = cv2.imencode('*.jpg', \
-                                        numpy.concatenate((frame, mask & frame), axis=0))
+##                    ret, self.imageBuf = cv2.imencode('*.jpg', \
+##                                        numpy.concatenate((frame, colorSal), axis=0))
                     self.isUpdated = True
 
     def autoExposure(self, yuv_frame):
-        lumaAvg = numpy.average(yuv_frame[:,:,0])
-        self.avg.append(lumaAvg) 
-        avg = self.avg.avg()
-        print(avg, lumaAvg, self.getExposure())
-        if (avg > 180):
-            self.decExposure()
-        if (avg < 130):
-            self.incExposure()
+        self.skipCounter = self.skipCounter-1
+        if (self.skipCounter < 1):
+            lumaAvg = numpy.average(yuv_frame[:,:,0])
+            print(lumaAvg, self.getExposure())
+            if (lumaAvg > 190) or (lumaAvg < 160):
+                self.skipCounter = 2
+                newExposure = 175*self.getExposure()/lumaAvg
+                print("newExposure: %f" % newExposure)
+                if (newExposure < 1):
+                    self.feed.set(cv2.CAP_PROP_EXPOSURE, newExposure)
 
     def getExposure(self):
         exp = self.feed.get(cv2.CAP_PROP_EXPOSURE)
@@ -87,20 +95,10 @@ class CV:
         if exp >= 0:
             self.feed.set(cv2.CAP_PROP_EXPOSURE, exp)
 
-
-from collections import deque
-
-class DequeAverager(deque):
-
-    def __init__(self, size) -> None:
-        super().__init__()
-        self.stackSize = size
-
-    def append(self, object_t) -> None:
-        super().append(object_t)
-        if len(self) > self.stackSize:
-            self.popleft()
-
-    def avg(self):
-        return numpy.mean(self)
-
+    def countFPS(self):
+        if(self.time == int(time.time())):
+            self.fps += 1
+        else:
+            print('FPS: %i' % (self.fps))
+            self.time = int(time.time())
+            self.fps=0
