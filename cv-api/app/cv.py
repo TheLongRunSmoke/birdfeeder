651451c2b3c47c58b8a8b22ef6675395391f8588
thelongrunsmoke <thelongrunsmoke@gmail.com>
2017-12-28 08:20:52 +0500

    MOG test

--- cv-api/app/cv.py
+++ cv-api/app/cv.py
@@ -12,13 +12,17 @@ class CV:
         self.feed = cv2.VideoCapture(0)
         self.feed.set(cv2.CAP_PROP_FRAME_WIDTH,800)
         self.feed.set(cv2.CAP_PROP_FRAME_HEIGHT,600)
-        self.feed.set(cv2.CAP_PROP_SHARPNESS,1)
+        # self.feed.set(cv2.CAP_PROP_SHARPNESS,1)
         self.feed.set(cv2.CAP_PROP_AUTO_EXPOSURE,0.25) # manual mode
         self.feed.set(cv2.CAP_PROP_EXPOSURE, 0)
         self.isUpdated = False
         self.time = 0
         self.fps = 0
         self.streamBuf = None
+        self.avg = DequeAverager(3)
+        
+        self.bgSubtractor = cv2.createBackgroundSubtractorMOG2()
+        self.history = 100
         
         self.loop = asyncio.get_event_loop()
         self.pool = ThreadPoolExecutor(max_workers=multiprocessing.cpu_count())
@@ -45,20 +49,29 @@ class CV:
             
                 rval, frame = self.feed.retrieve()
 
-                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                self.autoExposure(cv2.cvtColor(frame, cv2.COLOR_BGR2YUV))
 
-                yuv = cv2.cvtColor(frame, cv2.COLOR_BGR2YUV)
+                #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-                lumaAvg = numpy.average(yuv[:,:,0])              
-                print(self.getExposure(), lumaAvg)
-                if (lumaAvg > 160):
-                    self.decExposure()
-                if (lumaAvg < 120):
-                    self.incExposure()
+                mask = self.bgSubtractor.apply(frame, learningRate=1.0/self.history)
+                mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+                
                                 
                 if not self.isUpdated:
                     ret, self.imageBuf = cv2.imencode('*.jpg', frame)
+                    ret, self.imageBuf = cv2.imencode('*.jpg', \
+                                        numpy.concatenate((frame, mask & frame), axis=0))
                     self.isUpdated = True
+
+    def autoExposure(self, yuv_frame):
+        lumaAvg = numpy.average(yuv_frame[:,:,0])
+        self.avg.append(lumaAvg) 
+        avg = self.avg.avg()
+        print(avg, lumaAvg, self.getExposure())
+        if (avg > 180):
+            self.decExposure()
+        if (avg < 130):
+            self.incExposure()
 
     def getExposure(self):
         exp = self.feed.get(cv2.CAP_PROP_EXPOSURE)
@@ -66,13 +79,28 @@ class CV:
 
     def incExposure(self):
         exp = self.getExposure() + 0.0003
-        print('inc')
         if exp <= 1:
             self.feed.set(cv2.CAP_PROP_EXPOSURE, exp)
             
     def decExposure(self):
         exp = self.getExposure() - 0.0003
-        print('dec')
         if exp >= 0:
             self.feed.set(cv2.CAP_PROP_EXPOSURE, exp)
- 
+
+
+from collections import deque
+
+class DequeAverager(deque):
+
+    def __init__(self, size) -> None:
+        super().__init__()
+        self.stackSize = size
+
+    def append(self, object_t) -> None:
+        super().append(object_t)
+        if len(self) > self.stackSize:
+            self.popleft()
+
+    def avg(self):
+        return numpy.mean(self)
+
